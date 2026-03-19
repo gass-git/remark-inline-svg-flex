29f96b69d4cc0ccf26f5c4ecbf7d9f0af1875eff
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-03-19 09:21:31 +0100

    refactor: plugin.ts cleanup

--- src/plugin.ts
+++ src/plugin.ts
@@ -6,30 +6,27 @@ import { visit } from 'unist-util-visit';
 import { VFile } from 'vfile';
 import { optimize } from 'svgo';
 import type { Output } from 'svgo';
-
-type Options = {
-  assetsDir: string | undefined;
-  wrapper: string;
-};
+import type { Options } from './types';
 
 const inlineSvg: Plugin<[string?, string?], Root, Root> = (
+  suffix = '.svg',
   assetsDir = undefined,
-  wrapper = `<figure class="inline-svg"></figure>`,
+  wrapper = '<figure class="inline-svg"></figure>',
 ) => {
-  const options: Options = { assetsDir, wrapper };
+  const options: Options = { suffix, assetsDir, wrapper };
 
   return function transformer(tree: Root, file: VFile): void {
-    visit(tree, 'image', (node: Image, index: number, parent: any | undefined) => {
-      if (!node.url?.endsWith('.svg') || index == null || !parent) return;
+    visit(tree, 'image', (node, i, parent) => {
+      if (!node.url?.endsWith(suffix) || !parent) return;
 
       const markdownFileDir = path.dirname(file.history[0]);
 
       try {
-        const svgPath = resolvePath(options, node, markdownFileDir);
+        const svgPath = resolvePath(options.assetsDir, node, markdownFileDir);
         const svgString = fs.readFileSync(svgPath, 'utf8');
         const optimizedSvgString = optimizeSvg(svgString).data;
 
-        parent.children[index] = {
+        parent.children[i] = {
           type: 'html',
           value: wrap(optimizedSvgString, wrapper),
         };
@@ -51,13 +48,13 @@ function wrap(content: string, customHtmlWrapper: string): string {
   return w.slice(0, i) + content + w.slice(i);
 }
 
-function resolvePath(options: Options, node: Image, markdownFileDir: string): string {
+function resolvePath(assetsDir: string, node: Image, markdownFileDir: string): string {
   if (path.isAbsolute(node.url)) {
     // Treat as relative to project root.
     return path.resolve(process.cwd(), node.url);
-  } else if (options.assetsDir) {
+  } else if (assetsDir) {
     // If path is not absolute, use the assets directory provided in options.
-    return path.resolve(process.cwd(), options.assetsDir, node.url);
+    return path.resolve(process.cwd(), assetsDir, node.url);
   } else {
     // Treat as relative to markdown file directory.
     return path.resolve(markdownFileDir, node.url);
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export type Options = {
+  suffix: string;
+  assetsDir: string | undefined;
+  wrapper: string;
+};
